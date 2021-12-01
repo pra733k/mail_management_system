8469b809523072852d77fb70fefd2f0faa2aef02
pra733k <prateekbhatia2027@gmail.com>
2021-12-01 23:17:56 +0530

    Update mail.py

--- mail.py
+++ mail.py
@@ -5,9 +5,14 @@ s.starttls() # start TLS for security
 # sid = input('Enter Sender Email Id: ')
 # spass = input('Enter Sender Email Password: ')
 s.login("18bec045@smvdu.ac.in", "prateek0690") # Authentication - Enter Sender Id & Password
-rid = input("Enter receiver Id: ")
 subject = input("Enter your subject here: ")
 message = input("Enter your message here: ")
 msg = f'Subject: {subject}\n\n{message}'
-s.sendmail("18bec045@smvdu.ac.in", rid, msg) # sending the mail
+lirec=[]
+x = int(input("Enter no. of recipients: "))
+for k in range(x):
+	rid = input("Enter receiver Id: ")
+	lirec.append(rid)
+for i in lirec:
+	s.sendmail("18bec045@smvdu.ac.in", i, msg) # sending the mail
 s.quit()  # terminating the session
