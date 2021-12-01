324a0e28b258920d4b7325a84b38cfbad15028bf
pra733k <prateekbhatia2027@gmail.com>
2021-12-01 23:38:00 +0530

    Update mail.py

--- mail.py
+++ mail.py
@@ -2,9 +2,9 @@
 import smtplib
 s = smtplib.SMTP('smtp.gmail.com', 587) # creates SMTP session
 s.starttls() # start TLS for security
-# sid = input('Enter Sender Email Id: ')
-# spass = input('Enter Sender Email Password: ')
-s.login("18bec045@smvdu.ac.in", "prateek0690") # Authentication - Enter Sender Id & Password
+sid = input('Enter Sender Email Id: ')
+spass = input('Enter Sender Email Password: ')
+s.login(sid, spass) # Authentication - Enter Sender Id & Password
 subject = input("Enter your subject here: ")
 message = input("Enter your message here: ")
 msg = f'Subject: {subject}\n\n{message}'
