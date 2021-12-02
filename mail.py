fb055cac8220a7f5ca38f417fb481ee6f0750c6a
pra733k <prateekbhatia2027@gmail.com>
2021-12-02 12:06:45 +0530

    Update mail.py

--- mail.py
+++ mail.py
@@ -1,18 +1,32 @@
-# Python code to illustrate Sending mail
+import flask
 import smtplib
+
 s = smtplib.SMTP('smtp.gmail.com', 587) # creates SMTP session
 s.starttls() # start TLS for security
+
+app = Flask(__name__, template_folder='templates')
+app.config['SQLALCHEMY_DATABASE_URI'] = base_url
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
 sid = input('Enter Sender Email Id: ')
 spass = input('Enter Sender Email Password: ')
 s.login(sid, spass) # Authentication - Enter Sender Id & Password
+
 subject = input("Enter your subject here: ")
 message = input("Enter your message here: ")
 msg = f'Subject: {subject}\n\n{message}'
+
 lirec=[]
 x = int(input("Enter no. of recipients: "))
+
 for k in range(x):
 	rid = input("Enter receiver Id "+str(k+1)+" : ")
 	lirec.append(rid)
 for i in lirec:
-	s.sendmail("18bec045@smvdu.ac.in", i, msg) # sending the mail
+	s.sendmail(sid, i, msg) # sending the mail
+
 s.quit()  # terminating the session
+
+if __name__ == "__main__":
+	app.run(use_reloader=True, debug=True)
