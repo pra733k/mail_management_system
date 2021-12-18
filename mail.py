10d081e869c4ec87eab47852de69a4d539d11636
pra733k <prateekbhatia2027@gmail.com>
2021-12-18 19:46:45 +0530

    none

--- mail.py
+++ mail.py
@@ -1,31 +1,39 @@
-from flask import Flask
+from flask import Flask, redirect, render_template, request, session, abort, flash , Response, request
+from flask_wtf import FlaskForm
+from form import SignUpForm
+from flask import *
 import smtplib
+import re
 
+app = Flask(__name__, template_folder='templates')
+app.config['SECTERT_KEY']='mysecretkey'
 s = smtplib.SMTP('smtp.gmail.com', 587) # creates SMTP session
 s.starttls() # start TLS for security
 
-app = Flask(__name__, template_folder='templates')
+@app.route("/", methods=['GET','POST'])
+def login():
+	form = SignUpForm()
+	if form.is_submitted():
+		sid = request.form()
 
+	if request.method == "POST":
+		sid = request.form.get("sid")
+	# if(re.search('@gmail.com$', str(sid))):
+		return render_template("sid.html",sid = sid)
+	else :
+		flash('ID NOT ACCEPTED')
+		redirect("/") #redirect to login page 
+	
+	# spass = request.form.get('spass')
+	# try:
+	# 	s.login(sid, spass) # Authentication - Enter Sender Id & Password
+	# 	flash('ID & PASS ACCEPTED')
+	# 	return redirect('www.google.com')
+	# except Exception as e:
+	# 	return Response('Error: {}'.format(str(e)), status=500)
+	# return Response(result, status=200)
 
-print("Please Enable 'Less secure app access' in your E-mail Account.\n")
-sid = input('Enter Sender Email Id: ')
-spass = input('Enter Sender Email Password: ')
-s.login(sid, spass) # Authentication - Enter Sender Id & Password
-
-subject = input("Enter your subject here: ")
-message = input("Enter your message here: ")
-msg = f'Subject: {subject}\n\n{message}'
-
-lirec=[]
-x = int(input("Enter no. of recipients: "))
-
-for k in range(x):
-	rid = input("Enter receiver Id "+str(k+1)+" : ")
-	lirec.append(rid)
-for i in lirec:
-	s.sendmail(sid, i, msg) # sending the mail
-
-s.quit()  # terminating the session
 
 if __name__ == "__main__":
-	app.run(use_reloader=True, debug=True)
+	app.secret_key = 'super secret key'
+	app.run(debug=True)
